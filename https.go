c562e81e052dcabc6f7bfe440f6a519871b0e6ab
Gaurav Gogia <DesmondANIMUS@users.noreply.github.com>
2018-01-11 16:50:55 +0000

    Add files via upload

---
+++ https.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/sessions"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("./*.html"))
+}
+
+func main() {
+	http.Handle("/assets/",
+		http.StripPrefix("/assets",
+			http.FileServer(http.Dir("./assets"))))
+
+	http.HandleFunc("/", index)
+
+	go http.ListenAndServe(":5000", nil)
+	err := http.ListenAndServeTLS(":8888", "cert.pem", "key.pem", nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session")
+
+	if r.Method == http.MethodPost {
+		if r.FormValue("email") != "" {
+			session.Values["email"] = r.FormValue("email")
+		}
+		session.Save(r, w)
+	}
+
+	if pusher, ok := w.(http.Pusher); ok {
+		options := &http.PushOptions{
+			Header: http.Header{
+				"Accept-Encoding": r.Header["Accept-Encoding"],
+			},
+		}
+		if err := pusher.Push("/assets/js/login.js", options); err != nil {
+			fmt.Println(err)
+		}
+		if err := pusher.Push("/assets/css/normalizeLogin.css", options); err != nil {
+			fmt.Println(err)
+		}
+		if err := pusher.Push("/assets/css/styleLogin.css", options); err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		fmt.Println("COULD NOT PUSH")
+	}
+
+	tpl.ExecuteTemplate(w, "cook.html", nil)
+	log.Println(r.URL.Path)
+}
+
+var store = sessions.NewCookieStore([]byte("something-very-secret"))
+
+/*
+   Process for handling TLS Secure Connections for your WebServices:
+   1. go run C:\Go\src\crypto\tls\generate_cert.go --host=<domain_name>
+      Do that to generate certificate & key or use StartSSL or something.
+   2. Hand-over your certificate to another company which will sign them.
+
+   Otherwise you will have to go to some Certificate Authority and get
+   your Certificate signed.
+
+   Cloudflare provides free SSL / TLS, self-signed certificates work there
+
+   One of the free companies is https://letsencrypt.org/
+*/
